680cb1d077648890bc9e51fedca73dc33a099448
Manas Malla <manasmalla.dev@gmail.com>
2026-02-02 00:00:07 +0530

    Update links.js

--- links.js
+++ links.js
@@ -1,7 +1,7 @@
 // Download links configuration - Update these URLs easily
 const downloadLinks = {
   macos: "https://github.com/TheAnanta/chitti-app/releases/download/5.0.1/chitti-2025-5.0.1-mac.dmg",
-  windows: "https://github.com/TheAnanta/chitti-app/releases/download/4.0.1/chitti.exe"
+  windows: "https://github.com/TheAnanta/chitti-app/releases/download/5.0.1/chitti.exe"
 };
 
 // Export for use in different environments
