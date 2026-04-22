f08cb8bd37dec17eb4550cd8462268ebd84e0f62
Jagadeesh Potupureddy <142223483+deadhearth01@users.noreply.github.com>
2026-04-22 23:19:32 +0530

    Update Windows download link to version 5.0.6

--- links.js
+++ links.js
@@ -1,7 +1,7 @@
 // Download links configuration - Update these URLs easily
 const downloadLinks = {
   macos: "https://github.com/TheAnanta/chitti-app/releases/download/5.0.5/chitti-2025-5.0.5-mac.dmg",
-  windows: "https://github.com/TheAnanta/chitti-app/releases/download/5.0.5/chitti.exe"
+  windows: "https://github.com/TheAnanta/chitti-app/releases/download/5.0.6/chitti.exe"
 };
 
 // Export for use in different environments
